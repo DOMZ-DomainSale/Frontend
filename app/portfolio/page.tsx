d3149303bf7741e213b8f738e6416f41b8edb8bd
samiramrullah <samiramrullah@gmail.com>
2025-11-11 18:24:06 +0530

    portfolio design final

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,7 +5,7 @@ import NavbarComponenet from '@/components/NavbarComponenet'
 const page = () => {
     return (
         <div className='lg:pl-[10%] lg:pr-[10%] lg:pt-9'>
-            <NavbarComponenet colorText="S" plainText="ignUp" IsParaText={true} ParaText="Your paragraph text here" />
+            <NavbarComponenet colorText="MY " plainText="Portfolio" IsParaText={true} ParaText="Browse a commission-free catalog and connect directly with domain owners." />
             <div className="w-full max-w-5xl mx-auto mt-8">
                 <div className="flex justify-between mb-2">
                     <button className="rounded-full border border-gray-300 px-4 py-1 bg-white text-gray-700 shadow hover:bg-gray-100">
