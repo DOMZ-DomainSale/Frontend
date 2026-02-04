ad95321929b397317ba3ff02ddc98188ef8ffa30
Samiramrullah <samiramrullah@gmail.com>
2026-02-04 22:45:43 +0530

    buy text remoed

--- app/domainbuy/DomainBuyClient.tsx
+++ app/domainbuy/DomainBuyClient.tsx
@@ -30,17 +30,17 @@ return (
 
       <DomainTable searchQuery={searchQuery} />
 
-      <div className="w-full px-2 py-12 bg-blue-50 flex flex-col items-center rounded-3xl">
+      {/* <div className="w-full px-2 py-12 bg-blue-50 flex flex-col items-center rounded-3xl">
         <h2 className="text-2xl md:text-3xl font-light text-center mb-2 mt-2">
           <span className="text-blue-600 font-semibold">How </span>
           <span className="font-semibold">Domz.com</span>
           <span className="font-normal"> Benefits Both Sellers and Buyers</span>
-        </h2>
+        </h2> */}
 
         {/* <p className="text-base text-gray-600 text-center max-w-2xl mb-8">
           Our domain catalog is designed to remove the friction of traditional marketplaces that frustrates both sides.
         </p> */}
-      </div>
+      {/* </div> */}
 
       <Footer />
     </div>
