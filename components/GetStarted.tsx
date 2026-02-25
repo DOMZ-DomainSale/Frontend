3c54f80bd67b1d1048bfdb2eb3b9ef2d63a853bf
Samiramrullah <samiramrullah@gmail.com>
2026-02-25 22:17:57 +0530

    updated

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -1,16 +1,16 @@
 "use client";
 import Subscribe from "@/utils/subscribe";
-// import QuickConectCard from "./cards/QuickConectCard";
+import QuickConectCard from "./cards/QuickConectCard";
 
 const GetStarted = () => {
   return (
     <>
-      {/* <QuickConectCard
+      <QuickConectCard
         title="List with Domz" description="Choose the plan that fits you best — flexible, seller-first pricing."
         mainButton="Get Started"
         subButton={true}
-      /> */}
-       <Subscribe  buttonText="Get Started" heading="List With Domz" text="Partner with us and showcase your portfolio."/>
+      />
+       {/* <Subscribe  buttonText="Get Started" heading="List With Domz" text="Partner with us and showcase your portfolio."/> */}
     </>
 
   );
